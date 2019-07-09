31cc73f9a5dec99e6ffbb86b728ebfe46f33ab8a
Mikhail <misha1997mtl@gmail.com>
2019-07-09 13:32:35 +0400

    v.0.2

--- app/src/main/java/com/topstrejiok/changemanager/adapter/NameAdapter.java
+++ app/src/main/java/com/topstrejiok/changemanager/adapter/NameAdapter.java
@@ -9,8 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.topstrejiok.changemanager.R;
@@ -150,8 +150,8 @@ public class NameAdapter extends RecyclerView.Adapter<NameAdapter.NameViewHolder
     static class NameViewHolder extends RecyclerView.ViewHolder {
 
         TextView name;
-        ImageView delete;
-        ImageView edit;
+        Button delete;
+        Button edit;
         EditText donate;
 
         public NameViewHolder(@NonNull View itemView) {
